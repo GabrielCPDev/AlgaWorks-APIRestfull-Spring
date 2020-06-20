aab107161d0c3f5be714efb437aa2de15413aef3
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2020-06-20 12:00:01 -0300

    commit inicial

---
+++ src/main/java/com/gabriel/ordemDeServicoAPI/ordemDeServicoAPI/OrdemDeServicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.ordemDeServicoAPI.ordemDeServicoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemDeServicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemDeServicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/ordemDeServicoAPI/ordemDeServicoAPI/OrdemDeServicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.ordemDeServicoAPI.ordemDeServicoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemDeServicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
